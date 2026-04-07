550bf26bdf3863ddd9e54db360d882b352906c85
Gregory Jefferis <jefferis@gmail.com>
2026-04-07 11:45:29 +0100

    R CMD check wants startup messages in onAttach
    * this is a bit convoluted but does address the issue

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
+.yakuba_state <- new.env(parent = emptyenv())
+
 .onLoad <- function(libname, pkgname) {
   op.yakuba <- list(
     yakuba.dataset = "yakuba"
@@ -8,8 +10,17 @@
     options(op.yakuba[toset])
   }
 
+  .yakuba_state$xform_registration_failed <- FALSE
   res <- try(yakuba_register_xforms(), silent = TRUE)
   if (inherits(res, "try-error")) {
+    .yakuba_state$xform_registration_failed <- TRUE
+  }
+
+  invisible()
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (isTRUE(.yakuba_state$xform_registration_failed)) {
     packageStartupMessage(
       "Trouble registering yakuba xforms.\n",
       "Try `yakuba_register_xforms()` after loading `nat.templatebrains`."
